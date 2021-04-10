db0d0a32a70f881f49b968e02ccbea0b25002cd8
Ritik Gupta <46594309+ritik11ritik@users.noreply.github.com>
2021-04-10 14:48:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  9 10:56:15 2021
+
+@author: rg
+"""
+import Download
+
+def main():
+    Download.download_song()
+    
+if __name__ == '__main__':
+    main()
